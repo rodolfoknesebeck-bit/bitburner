3dfc3f7038b42bcd2540cdb89b0cbab39710fe51
rodolfoknesebeck-bit <rodolfo.knesebeck@gmail.com>
2026-04-13 14:09:26 -0300

    Run starterServers.js in main function

--- starter.js
+++ starter.js
@@ -1,6 +1,8 @@
 //       runs whatever script is hardcodded. seems redundant for now but its nice to have for when have lots of scripts to run when starting a node
 
 /** @param {NS} ns */
+/** @param {NS} ns */
 export async function main(ns) {
 ns.run("hack/threadcounter.js");
+ns.run("servers/starterServers.js");
 }
